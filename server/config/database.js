458ad6a0c2be240ddfdf86ae004ce18631a2eb71
himanshu101 <himanshukumar.connect@gmail.com>
2025-05-03 14:53:09 +0530

    trying to fix backend

--- server/config/database.js
+++ server/config/database.js
@@ -4,8 +4,6 @@ require("dotenv").config();
 exports.connect = () => {
 
     mongoose.connect(process.env.MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology:true,
     })
     .then(() => console.log("DB Connected Successfully"))
     .catch( (error) => {
